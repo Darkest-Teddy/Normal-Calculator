c0601e40b32155685bbc4ea0f4de8c16b402aed9
Jack He <138162567+Darkest-Teddy@users.noreply.github.com>
2025-03-15 16:14:31 -0400

    funk button added

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ arr.forEach(button => {
     button.addEventListener('click', (e) =>{
         if(e.target.innerHTML == '='){
             string = eval(string);
-            input.value = ((parseInt(string) + (Math.random() * (0.5 - 0.1) + 0.1)).toFixed(2).toString()) + responses[Math.floor(Math.random() * responses.length)];
+            randomAnswer = ((parseInt(string) + (Math.random() * (0.5 - 0.1) + 0.1)).toFixed(2).toString())
+
+            input.value = randomAnswer + responses[Math.floor(Math.random() * responses.length)];
+            string = randomAnswer;
         }
 
         else if(e.target.innerHTML == 'AC'){
@@ -17,6 +20,14 @@ arr.forEach(button => {
         }
         else if(e.target.innerHTML == 'DEL'){
             string = string.substring(0, string.length-1);
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'chill'){
+            string = (Math.random() * (999 - 0.1) + 0.1).toFixed(2); 
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'funk'){
+            string = "┏(-_-)┛";
             input.value = string;
         }
         else{
